610376d91870fbd06a257f12b68fa1cffb24b4be
aghashan <96901712+aghashan@users.noreply.github.com>
2022-01-24 16:14:57 +0700

    Add files via upload

---
+++ M-TRONIK/src/m/tronik/MTRONIK.java
@@ -0,0 +1,20 @@
+
+package m.tronik;
+//import java.util.Scanner;
+
+public class MTRONIK {
+   
+   
+    public static void main(String[] args) {
+        int total;
+       DEPAN ObjekInput=new DEPAN();
+                 ObjekInput.pilih();
+      
+             }
+     
+          }
+
+   
+    
+
+
